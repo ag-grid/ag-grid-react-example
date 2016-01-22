52f13438d180c482c077a084315040b3b0bb479a
Ceolter <niall.crosby@gmail.com>
2016-01-22 06:59:17 +0000

    initial checkin

---
+++ webpack.config1.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.js$|\.jsx$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
