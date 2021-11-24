b1eeabacc1279161a1705f0ea3e55f538211456e
Guilherme Lopes <guilherme@gwm-solutions.com>
2021-11-24 10:05:52 -0800

    AG-6048 - remove IE specific code

--- src-examples/richGridDeclarativeExample/NameCellEditor.jsx
+++ src-examples/richGridDeclarativeExample/NameCellEditor.jsx
@@ -73,14 +73,16 @@ export default class NameCellEditor extends React.Component {
     afterGuiAttached() {
         // get ref from React component
         const eInput = this.refs.textField;
+
         eInput.focus();
+
         if (this.highlightAllOnFocus) {
             eInput.select();
         } else {
-            // when we started editing, we want the carot at the end, not the start.
-            // this comes into play in two scenarios: a) when user hits F2 and b)
-            // when user hits a printable character, then on IE (and only IE) the carot
-            // was placed after the first character, thus 'apply' would end up as 'pplea'
+            // when we started editing, we want the caret at the end, not the start.
+            // this comes into play in two scenarios: 
+            //   a) when user hits F2 
+            //   b) when user hits a printable character
             const length = eInput.value ? eInput.value.length : 0;
             if (length > 0) {
                 eInput.setSelectionRange(length, length);
