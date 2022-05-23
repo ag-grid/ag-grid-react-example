f00f32e17469694ca11b3e3b52524597df2f5491
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-23 16:51:01 +0800

    Delete src-trader-dashboard directory

--- src-trader-dashboard/services/StoreService.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from "redux";
-
-import fxData from "../reducers/fxDataReducer.jsx";
-
-class StoreService {
-}
-StoreService.STORE = createStore(fxData);
-export default StoreService;
