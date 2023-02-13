5047195b04cd4d3103d120860327196ffff43faa
Alan Treadway <alan.treadway@ag-grid.com>
2023-02-13 11:03:40 +0000

    Log tsc errors during npm test.

--- ts-tests/runTsTests.sh
+++ ts-tests/runTsTests.sh
@@ -6,7 +6,7 @@
 error_found=false
 
 # has an invalid property - should complain
-./node_modules/.bin/tsc --target "ES5" --module 'commonjs' --lib esnext,dom --allowSyntheticDefaultImports --jsx 'preserve' --noEmit --strict ts-tests/InvalidGridProperty.tsx &> /dev/null
+./node_modules/.bin/tsc --target "ES5" --module 'commonjs' --lib esnext,dom --allowSyntheticDefaultImports --jsx 'preserve' --noEmit --strict ts-tests/InvalidGridProperty.tsx
 
 if [ $? -eq 0 ]; then
     echo "ag-grid-react grid with invalid property should throw a compiler error"
@@ -14,7 +14,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # a valid grid - no errors should be emitted
-./node_modules/.bin/tsc --target "ES5" --module 'commonjs' --lib esnext,dom --allowSyntheticDefaultImports --jsx 'preserve' --noEmit --strict ts-tests/SimpleFunctionalGrid.tsx &> /dev/null
+./node_modules/.bin/tsc --target "ES5" --module 'commonjs' --lib esnext,dom --allowSyntheticDefaultImports --jsx 'preserve' --noEmit --strict ts-tests/SimpleFunctionalGrid.tsx
 if [ $? -ne 0 ]; then
     echo "valid ag-grid-react grid should compile"
     error_found=true
